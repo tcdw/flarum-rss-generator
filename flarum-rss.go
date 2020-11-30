2b892bf7cd63bdbeaa2840eb2a636200f15eba2b
tcdw <tcdw2011@gmail.com>
2020-11-30 12:30:43 +0800

    数据获取

--- flarum-rss.go
+++ flarum-rss.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"github.com/buger/jsonparser"
 	"github.com/jessevdk/go-flags"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
-	"github.com/buger/jsonparser"
 )
 
 func fatalError(err error) {
@@ -15,6 +16,57 @@ func fatalError(err error) {
 		fmt.Printf("Fatal error: %s\n", err.Error())
 		os.Exit(1)
 	}
+}
+
+func getMeta(url string) (title string, description string, err error) {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return "", "", err
+	}
+
+	req.Header.Set("User-Agent", "Flarum_RSS_Bot/1.0")
+	res, err := client.Do(req)
+	if err != nil {
+		return "", "", err
+	}
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		return "", "", err
+	}
+
+	siteTitle := doc.Find("head > title").Text()
+	siteDescription := ""
+	siteDescriptionElement := doc.Find("head > meta[name=description]")
+	siteDescriptionData, exists := siteDescriptionElement.Attr("content")
+	if exists {
+		siteDescription = siteDescriptionData
+	}
+	return siteTitle, siteDescription, nil
+}
+
+func getThreads(url string) (data []byte, err error) {
+	fullURL := fmt.Sprintf("%s/api/discussions?include=user%%2ClastPostedUser%%2Ctags%%2CfirstPost&sort=-createdAt&page%%5Boffset%%5D", url)
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", fullURL, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("User-Agent", "Flarum_RSS_Bot/1.0")
+
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	content, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return content, nil
 }
 
 func main() {
@@ -28,19 +80,43 @@ func main() {
 		os.Exit(1)
 	}
 
-	// 下载原始数据
-	fullURL := fmt.Sprintf("%s/api/discussions?include=user%%2ClastPostedUser%%2Ctags%%2CfirstPost&sort=-createdAt&page%%5Boffset%%5D", opts.Site)
-	req, err := http.NewRequest("GET", fullURL, nil)
+	log.Println("Retrieving site meta data")
+	title, description, err := getMeta(opts.Site)
+	fatalError(err)
+	log.Printf("Title: %s\n", title)
+	log.Printf("Description: %s\n", description)
+
+	log.Println("Retrieving thread list")
+	data, err := getThreads(opts.Site)
 	fatalError(err)
 
-	req.Header.Set("User-Agent", "Flarum_RSS_Bot/1.0")
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
+	_, err = jsonparser.ArrayEach(data, func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
+		// 获取基本信息
+		postID, _ := jsonparser.GetString(value, "id")
+		title, _ := jsonparser.GetString(value, "attributes", "title")
+		createdAt, _ := jsonparser.GetString(value, "attributes", "createdAt")
+		userID, _ := jsonparser.GetString(value, "relationships", "user", "data", "id")
+		// 获取主题作者信息
+		var authorName string
+		var content string
+		_, _ = jsonparser.ArrayEach(data, func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
+			cType, _ := jsonparser.GetString(value, "type")
+			switch cType {
+			case "users":
+				cUserID, _ := jsonparser.GetString(value, "id")
+				if cUserID == userID {
+					authorName, _ = jsonparser.GetString(value, "attributes", "displayName")
+				}
+				break
+			case "posts":
+				cPostID, _ := jsonparser.GetString(value, "id")
+				if cPostID == postID {
+					content, _ = jsonparser.GetString(value, "attributes", "contentHtml")
+				}
+				break
+			}
+		}, "included")
+		fmt.Println(postID, title, createdAt, userID, authorName, content)
+	}, "data")
 	fatalError(err)
-
-	body, err := ioutil.ReadAll(resp.Body)
-	fatalError(err)
-
-	// WIP
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module flarum-rss-generator
 go 1.15
 
 require (
+	github.com/PuerkitoBio/goquery v1.6.0
 	github.com/buger/jsonparser v1.0.0
 	github.com/jessevdk/go-flags v1.4.0
 )
